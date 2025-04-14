4e0869811b462cd89592a9cf5efb388d7b4a33ae
Bouraffa <aymen.bouraffa@gmail.com>
2025-04-14 12:09:55 +0100

    Setting the version of AWS in Terraform

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.94.1"
+    }
+  }
+  # Creating VPC
+resource "aws_vpc" "wordpress-vpc" {
+  cidr_block = "10.0.0.0/16"
+  }
+}
