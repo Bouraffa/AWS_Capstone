85b8ba1028644a97baa35a61372cc0e508d3e1f2
Bouraffa <aymen.bouraffa@gmail.com>
2025-05-10 20:10:54 +0200

    modify outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
 output "wordpress_public_ip" {
-  value = aws_instance.wordpress_primary.public_ip
+  value = aws_instance.wordpress_public_ip
    description = "Public IP of the WordPress EC2 instance"
  
 }
