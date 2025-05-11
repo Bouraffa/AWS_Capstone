38793ec329b4896109a152c203906ff71700e881
Bouraffa <aymen.bouraffa@gmail.com>
2025-05-11 14:48:45 +0200

    Fix incorrect aws_instance reference in outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@
   # description = "Public IP of the WordPress EC2 instance"
  
 #}
+output "wordpress_public_ip" {
+  value       = aws_instance.wordpress_instance.public_ip
+  description = "Public IP of the WordPress EC2 instance"
+}
