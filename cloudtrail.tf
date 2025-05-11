692941172c52dda9526c9ce3c13b947f9f36c9b7
Bouraffa <aymen.bouraffa@gmail.com>
2025-05-11 20:01:08 +0200

    ADD Cloudtrail.tf

--- cloudtrail.tf
+++ cloudtrail.tf
@@ -1,16 +1,16 @@
-# resource "aws_s3_bucket" "cloudtrail_logs" {
-#   bucket = "cloudtrail-logs-${random_id.suffix.hex}"
-#   acl    = "private"
-# }
+resource "aws_s3_bucket" "cloudtrail_logs" {
+  bucket = "cloudtrail-logs-${random_id.suffix.hex}"
+  acl    = "private"
+}
 
-# resource "random_id" "suffix" {
-#   byte_length = 4
-# }
+resource "random_id" "suffix" {
+  byte_length = 4
+}
 
-# resource "aws_cloudtrail" "main" {
-#   name                          = "wordpress-trail"
-#   s3_bucket_name                = aws_s3_bucket.cloudtrail_logs.bucket
-#   include_global_service_events = true
-#   is_multi_region_trail        = true
-#   enable_log_file_validation   = true
-# }
+resource "aws_cloudtrail" "main" {
+  name                          = "wordpress-trail"
+  s3_bucket_name                = aws_s3_bucket.cloudtrail_logs.bucket
+  include_global_service_events = true
+  is_multi_region_trail        = true
+  enable_log_file_validation   = true
+}
